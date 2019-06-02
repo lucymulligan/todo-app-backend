ff921812ab2a0932be69e47bdebdf1aff8f2e1a4
lucymulligan <lucymulligan@hotmail.com>
2019-06-02 11:50:33 +0200

    Starting connection

--- tasks.js
+++ tasks.js
@@ -4,6 +4,13 @@ const app = express();
 const cors = require('cors');
 app.use(cors());
 
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: "tasksdb1"
+});
+
 app.get('/tasks', function (request, response) {
 
   const username = request.query.username;
